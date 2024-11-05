5eff6c6e914ed9a0dc392a37ac57d550d7dba674
rumkkee <accesareo@gmail.com>
2024-11-05 13:17:50 -0800

    added support for multiple lines of dialogue

--- Assets/Scripts/DialoguePanel.cs
+++ Assets/Scripts/DialoguePanel.cs
@@ -1,38 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class DialoguePanel : MonoBehaviour
 {
     // TODO: Make dialogue text scroll over time
 
+    public TextMeshProUGUI dialogueBox;
+
     public string speakerName;
 
     public List<string> dialogueStack;
+    public int currentIndex; // index of the current dialogue chunk that is loaded.
 
     public void OnActivate()
     {
         if (this.gameObject.activeInHierarchy) { return; }
-
+        ShowNextDialogue();
         gameObject.SetActive(true);
-        while (dialogueStack.Count > 0)
-        {
-            gameObject.SetActive(true);
-        }
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) 
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            Debug.Log("Pressed");
-            OnDeactivate();
+            if (dialogueStack.Count >= currentIndex + 1)
+            {
+                ShowNextDialogue();
+            }
+            else
+            {
+                OnDeactivate();
+            }
+        }
+
+    }
+
+    private void ShowNextDialogue()
+    {
+        currentIndex++;
+        if (dialogueStack.Count >= currentIndex)
+        {
+
+            dialogueBox.text = dialogueStack[currentIndex];
         }
     }
 
     public void OnDeactivate()
     {
         gameObject.SetActive(false);
+        currentIndex = -1;
     }
 
 }
