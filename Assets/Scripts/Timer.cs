4d6fd2632afb83d50f21823566442cc482f762a5
Trinity Stallworth <trinitystallworth57@gmail.com>
2024-11-12 01:17:06 -0800

    Implementing Timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+
+    //A float to reference the timer.
+    public float timeRemaining = 30f;
+    //Having the timer be set to the screen
+    public Text timderText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Start the timer
+        timerIsRunning = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timerIsRunning)
+        {
+            //Decreases the timer
+            if(timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+            }
+            else
+            {
+                //Will stop the timer
+                timeRemaining = 0;
+                timerIsRunning = false;
+                OnTimerEnd();
+            }
+        }
+        //Will update the timer text
+        UpdateTimerDisplay();
+    }
+
+    void UpdateTimerDisplay()
+    {
+        timerText.text = "Time Remaining: " + Mathf.Round(timeRemaining).ToString();
+    }
+
+    void OnTimerEnd()
+    {
+        Debug.Log("Timer is finished");
+    }
+}
