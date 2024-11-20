59161a43d99be3f0cac4d6332a77ae43b0687f22
Trinity Stallworth <trinitystallworth57@gmail.com>
2024-11-20 14:15:37 -0800

    Fixed some bugs and am now trying to implement.

--- Assets/Scripts/WireTask.cs
+++ Assets/Scripts/WireTask.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class WireTask : MonoBehaviour
 {
-
     public List<Color> _wireColors = new List<Color>();
     public List<Wire> _leftwires = new List<Wire>();
     public List<Wire> _rightwires = new List<Wire>();
 
     private List<Color> _availableColors;
-
     private List<int> _availableleftwiresIndex;
     private List<int> _availablerightwiresIndex;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,24 +19,27 @@ public class WireTask : MonoBehaviour
         _availableleftwiresIndex = new List<int>();
         _availablerightwiresIndex = new List<int>();
 
-        for(int i = 0; i < _leftwires.Count; i++)
+        for (int i = 0; i < _leftwires.Count; i++)
         {
             _availableleftwiresIndex.Add(i);
         }
-        for(int i = 0; i < _rightwires.Count; i++)
+        for (int i = 0; i < _rightwires.Count; i++)
         {
             _availablerightwiresIndex.Add(i);
         }
-        while(_availableColors.Count > 0 && _availableleftwiresIndex.Count > 0 && _availablerightwiresIndex.Count > 0)
+
+        while (_availableColors.Count > 0 && _availableleftwiresIndex.Count > 0 && _availablerightwiresIndex.Count > 0)
         {
             Color pickedColor = _availableColors[Random.Range(0, _availableColors.Count)];
             int pickedLeftIndex = Random.Range(0, _availableleftwiresIndex.Count);
-            int pickedRightIndex = Random.Range(0, _availablerightwiresIndex.Count); 
+            int pickedRightIndex = Random.Range(0, _availablerightwiresIndex.Count);
 
-            _leftwires[_availableleftwiresIndex(pickedLeftIndex)].SetColor(pickedColor);
-            _rightwires[_availablerightwiresIndex(pickedRightIndex)].SetColor(pickedColor);
+            // Assign the picked color to the wires at the chosen indices
+            _leftwires[_availableleftwiresIndex[pickedLeftIndex]].SetColor(pickedColor);
+            _rightwires[_availablerightwiresIndex[pickedRightIndex]].SetColor(pickedColor);
 
-            _availableColors.Remove(pickedColors);
+            // Remove the color and wire indices from the available list
+            _availableColors.Remove(pickedColor);
             _availableleftwiresIndex.RemoveAt(pickedLeftIndex);
             _availablerightwiresIndex.RemoveAt(pickedRightIndex);
         }
