b843a3ecf85773e7463fba3e8de7fc824a00ee47
Trinity Stallworth <trinitystallworth57@gmail.com>
2024-12-08 22:21:28 -0800

    Reestablisshed the variables according to the game description.

--- Assets/Scripts/BlowingBubble.cs
+++ Assets/Scripts/BlowingBubble.cs
@@ -5,5 +5,26 @@ using UnityEngine;
 //Blowing Bubble Minigame. This is to simulate the creation of the socket. 
 public class BlowingBubble : MonoBehaviour
 {
+    //The Variables for the Minigame 
+    //They will be used for implementing Dynamic Difficulty
+
+    //Declaring the timer variables to account for dynamic difficulty
+    public float timer = 10.0f;
+    private float originalTimer;
+    public float difficultyIncrease = 0.5f;
+
+    //To establish the growth rate of the bubble per button press
+    public float growthRate = 0.1f;
+
+    //Boolean to check to see if the Game has been won
+    private bool gameWon = false;
+
+    //Declaring the variables for the popup Window
+    public GameObject winPopup;
+    public GameObject lossPopup;
+
+
     
+
+
 }
